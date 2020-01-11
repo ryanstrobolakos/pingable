2a64d5c56bc52c3b798cecb65c44756badfdc337
Ryan Strobolakos <ryan.strobolakos@gmail.com>
2020-01-11 16:48:13 -0500

    adding clearEndpoints function when clear button is clicked

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", () => {
   const workspace = document.querySelector(".workspace");
   const createButton = document.querySelector(".create-button");
+  const clearButton = document.querySelector(".clear-button");
 
   createButton.addEventListener("click", function() {
     createEndpoint();
@@ -10,5 +11,12 @@ window.addEventListener("load", () => {
     const endpointButton = document.createElement("button");
     endpointButton.innerHTML = "Click to Configure";
     workspace.appendChild(endpointButton);
+
+    clearButton.addEventListener("click", function() {
+      const clearEndpoints = () => {
+        workspace.removeChild(endpointButton);
+      };
+      clearEndpoints();
+    });
   };
 });
