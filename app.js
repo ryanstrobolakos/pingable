9f449fc4a22fdf33a5bbc05efc40dfde00339000
Ryan Strobolakos <ryan.strobolakos@gmail.com>
2020-01-15 14:04:01 -0500

    adding default icon to endpoint, styling, and adding changeIcon function to update endpoint icon on click on each icon button

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ window.addEventListener("load", () => {
   const clickToConfigure_1 = document.querySelector("#click-to-configure-1");
   const submitInput_1 = document.querySelector("#submit-input-1");
 
+  const iconId_1 = document.querySelector("#iconId-1");
+
+  const website_1 = document.querySelector("#website-1");
+  const server_1 = document.querySelector("#server-1");
+  const computer_1 = document.querySelector("#computer-1");
+  const printer_1 = document.querySelector("#printer-1");
+  const networkDevice_1 = document.querySelector("#network-device-1");
+
   //changeColor, changeOpacity, changeDisplay functions to change styling
 
   function changeColor(object, value) {
@@ -68,6 +76,7 @@ window.addEventListener("load", () => {
     });
     changeDisplay(endpointBlock, "none");
     changeOpacity(configurationDetails, 0);
+    changeIcon(button, iconId_1, defaultIcon); //may need to change this to reset function once created
   }
 
   //calling activateEndpoint function on click of endpointButton
@@ -115,4 +124,25 @@ window.addEventListener("load", () => {
       changeOpacity(configure, 0);
     });
   }
+
+  //Change Icon functionality
+
+  const defaultIcon = "fas fa-question";
+  const websiteIcon = "far fa-address-card";
+  const serverIcon = "far fa-clone";
+  const computerIcon = "fas fa-laptop";
+  const printerIcon = "fas fa-print";
+  const networkDeviceIcon = "fas fa-arrows-alt";
+
+  function changeIcon(icon, iconId, value) {
+    icon.addEventListener("click", () => {
+      iconId.className = value;
+    });
+  }
+
+  changeIcon(website_1, iconId_1, websiteIcon);
+  changeIcon(server_1, iconId_1, serverIcon);
+  changeIcon(computer_1, iconId_1, computerIcon);
+  changeIcon(printer_1, iconId_1, printerIcon);
+  changeIcon(networkDevice_1, iconId_1, networkDeviceIcon);
 });
